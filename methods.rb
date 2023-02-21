0ac40372c56e08bee5eba5946d3c620df37df166
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-02-21 14:36:54 -0500

    commit tree 4251e232f1c624e143e414825790f0699d325cfb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+  puts 'Hello, programmer!'
+  "Hello, programmer!"
+end
+
+def greet(name)
+
+  puts "Hello, #{name}!"
+  
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add (num1,num2)
+  return num1 + num2
+end
+
+def halve(num)
+  if num.is_a?(Integer)
+  return num /2
+  else
+return nil
+end
+end
+puts halve(6)
